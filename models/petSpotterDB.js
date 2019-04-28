b24dc50e8c3cb2490ddd3680bd9f516044647201
Matthew Krieger <CSgrad2017@gmail.com>
2019-04-28 15:10:20 -0400

    Progress

---
+++ models/petSpotterDB.js
@@ -0,0 +1,52 @@
+const petSpotterDB = [
+    {image: "https://images.unsplash.com/photo-1475954704693-ac6850a71ee0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+        location: {
+            address: '170 Weston St.',
+            city: 'Hartford',
+            state: 'CT',
+            zip: '06120'},
+        description: "Very confused. No tags",
+    },
+    {image: "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/57504760_10161824978200475_1169629880871026688_n.jpg?_nc_cat=102&_nc_ht=scontent-lga3-1.xx&oh=f3dbd33fd0493f79f380dd61560e96c0&oe=5D77C40C",
+        location: {
+            address: '29 Charlotte St.',
+            city: 'Binghampton',
+            state: 'NY',
+            zip: '13905'},
+        description: "Friendly. No collar",
+    },
+    {image: "https://images.unsplash.com/photo-1553450011-54705ba130a0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+        location: {
+            address: '59 Sanford St.',
+            city: 'West Haven',
+            state: 'CT',
+            zip: '06516'},
+        description: "Lots of energy. No tags",
+    },
+    {image: "https://www.marleague.org/wp-content/uploads/2018/02/2014-2-14-MARL_DSC6466_preview.jpeg",
+        location: {
+            address: '630 Maplehill Ave.',
+            city: 'Lansing',
+            state: 'MI',
+            zip: '48910'},
+        description: "",
+    },
+    {image: "https://images.unsplash.com/photo-1423846729060-aa8072893e5c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+        location: {
+            address: '890 S. Santa Fe St.',
+            city: 'Hemet',
+            state: 'CA',
+            zip: '92543'},
+        description: "Collar with no ID. Very friendly",
+    },
+    {image: "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/57056184_10214838576420767_7219283749001756672_o.jpg?_nc_cat=107&_nc_ht=scontent-lga3-1.xx&oh=8b6368827c3595f270e9b447f23a9622&oe=5D732309",
+        location: {
+            address: '1650 Kuiper Lane',
+            city: 'Mt Pleasant',
+            state: 'WI',
+            zip: '53406'},
+        description: "Had fleas when I found her. Brought to vet.",
+    },
+];
+
+module.exports = petSpotterDB;
--- server.js
+++ server.js
@@ -1,32 +1,35 @@
 //___________________
 //Dependencies
 //___________________
-const express = require('express');
-const methodOverride  = require('method-override');
-const mongoose = require ('mongoose');
-const app = express ();
-const db = mongoose.connection;
+const express           =   require('express');
+const methodOverride    =   require('method-override');
+const mongoose          =   require ('mongoose');
+const app               =   express ();
+const db                =   mongoose.connection;
+const lostPets          =   require('./models/petSpotterDB.js');
+
+
 //___________________
 //Port
 //___________________
 // Allow use of Heroku's port or your own local port, depending on the environment
 const PORT = process.env.PORT || 5000;
 
-//___________________
-//Database
-//___________________
-// How to connect to the database either via heroku or locally
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/'+ `updateme`;
+// //___________________
+// //Database
+// //___________________
+// // How to connect to the database either via heroku or locally
+// const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/'+ `updateme`;
+//
+// // Connect to Mongo
+// mongoose.connect(MONGODB_URI ,  { useNewUrlParser: true});
 
-// Connect to Mongo
-mongoose.connect(MONGODB_URI ,  { useNewUrlParser: true});
-
-// Error / success
-db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
-db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
-db.on('disconnected', () => console.log('mongo disconnected'));
-// open the connection to mongo
-db.on('open' , ()=>{});
+// // Error / success
+// db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+// db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+// db.on('disconnected', () => console.log('mongo disconnected'));
+// // open the connection to mongo
+// db.on('open' , ()=>{});
 
 //___________________
 //Middleware
@@ -41,17 +44,23 @@ app.use(express.json());// returns middleware that only parses JSON - may or may
 
 //use method override
 app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
-
+app.set("view engine", "ejs");
 
 //___________________
 // Routes
 //___________________
 //localhost:3000
 app.get('/' , (req, res) => {
-    res.send('Hello World!');
+    res.render("landing");
 });
 
-//___________________
-//Listener
-//___________________
-app.listen(PORT, () => console.log( 'Listening on port:', PORT));
+app.get("/lostPets", (req, res) => {
+    res.render("index", {
+        lostPets: lostPets
+    });
+});
+
+// ___________________
+// Listener
+// ___________________
+app.listen(PORT, () => console.log(`The Pet Spotter Server Is Operational On Port: ${PORT}`));
